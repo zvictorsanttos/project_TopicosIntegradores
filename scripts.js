96e410139eed5618e6c79a7c9cc5ab6a1258251a
João Victor da Silva Santos <vitinn_santos@hotmail.com>
2021-10-31 02:47:24 -0300

    Atualização no codigo

--- scripts.js
+++ scripts.js
@@ -36,16 +36,58 @@ const transactions = [
 ];
 
 const Transaction = {
+  all: transactions,
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+
+    App.reload();
+  },
   // Entrada
-  incomes() {},
+  incomes() {
+    let income = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income = income + transaction.amount;
+      }
+    });
+
+    return income;
+  },
+
   // Saidas
-  expenses() {},
-  total() {},
+  expenses() {
+    let expenses = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expenses = expenses + transaction.amount;
+      }
+    });
+    return expenses;
+  },
+
+  // Total
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
 };
 
 const Utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? " - " : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
   },
 };
 
@@ -64,8 +106,8 @@ const DOM = {
     const html = `
     
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
-      <td class="data"${transaction.date}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
       <td>
           <img src="./assets/minus.svg" alt="Remover Transação">
       </td>
@@ -73,8 +115,44 @@ const DOM = {
 
     return html;
   },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
 };
 
-transactions.forEach(function (transaction) {
-  DOM.addTransaction(transaction);
+const App = {
+  init() {
+    Transaction.all.forEach((transaction) => {
+      DOM.addTransaction(transaction);
+    });
+
+    DOM.updateBalance();
+  },
+
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
+
+Transaction.add({
+  id: 50,
+  description: "Droga é o brain",
+  amount: 200,
+  date: "25/04/1998",
 });
